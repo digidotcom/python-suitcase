d8a5054068de116dc46436e0312558c3126195bd
Paul Osborne <Paul.Osborne@digi.com>
2012-02-20 21:08:52 -0600

    Update setup.py version

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import find_packages
 
 setup(
   name='pacman',
-  version='0.1',
+  version='0.1.1',
+  author="Paul Osborne",
+  author_email="paul.osborne@spectrumdsi.com",
   packages=find_packages(),
 )
