d539b014e1662f9300fb3ba24d1255ac16526249
Paul Osborne <Paul.Osborne@digi.com>
2012-02-20 20:40:33 -0600

    First version of pacman library with a example

---
+++ pacman/fields.py
@@ -0,0 +1,173 @@
+import struct
+
+class FieldProperty(object):
+    
+    def __init__(self, field, onget=None, onset=None):
+        self.onget = onget
+        self.onset = onset
+        self.field = field
+
+    def _default_onget(self, value):
+        return value
+
+    def _default_onset(self, value):
+        return value
+
+    def __get__(self, obj, objtype=None):
+        onget = self.onget
+        if onget is None:
+            onget = self.onget
+
+        value = self.field.__get__(self.field)
+        return onget(value)
+
+    def __set__(self, obj, value):
+        onset = self.onset
+        if onset is None:
+            onset = self._default_onset
+
+        self.field.__set__(self.field, onset(value))
+
+class BaseField(object):
+
+    # record the number of instantiations of fields.  This is how
+    # we can track the order of fields within a message
+    _global_seqno = 0
+
+    def __init__(self):
+        self._field_seqno = BaseField._global_seqno
+        self._value = None
+        BaseField._global_seqno += 1
+
+    def __repr__(self):
+        return repr(self._value)
+
+    def __get__(self, obj, objtype=None):
+        return self._value
+
+    def __set__(self, obj, value):
+        self._value = value
+
+
+class BaseStructField(BaseField):
+
+    def __init__(self):
+        BaseField.__init__(self)
+        self._value = None
+
+    def __len__(self):
+        return struct.calcsize(self.FORMAT)
+
+    def pack(self):
+        return struct.pack(self.FORMAT, self._value)
+
+    def unpack(self, data):
+        value = 0
+        for i, byte in enumerate(reversed(struct.unpack(self.FORMAT, data))):
+            value |= (byte << (i * 8))
+        self._value = value
+
+
+class BaseByteSequence(BaseField):
+
+    def __init__(self, num_bytes):
+        BaseField.__init__(self)
+        self.num_bytes = num_bytes
+        self.format = self.FORMAT_MODIFIER(num_bytes)
+
+    def __len__(self):
+        return self.num_bytes
+
+    def pack(self):
+        return struct.pack(self.format, *self._value)
+
+    def unpack(self, data):
+        self._value = struct.unpack(self.format, data)
+
+
+class BUByteSequence(BaseByteSequence):
+    FORMAT_MODIFIER = lambda self, l: ">" + "B" * l
+
+
+class LUByteSequence(BaseByteSequence):
+    FORMAT_MODIFIER = lambda self, l: "<" + "B" * l
+
+
+#===============================================================================
+# Unsigned Big Endian
+#===============================================================================
+class BUInt8(BaseStructField):
+    FORMAT = ">B"
+
+
+class BUInt16(BaseStructField):
+    FORMAT = ">H"
+
+
+class BUInt24(BaseStructField):
+    FORMAT = ">BBB"
+
+
+class BUInt32(BaseStructField):
+    FORMAT = ">I"
+
+
+class BUInt64(BaseStructField):
+    FORMAT = ">Q"
+
+
+#===============================================================================
+# Signed Big Endian
+#===============================================================================
+class BInt8(BaseStructField):
+    FORMAT = ">b"
+
+
+class BInt16(BaseStructField):
+    FORMAT = ">h"
+
+
+class BInt32(BaseStructField):
+    FORMAT = ">i"
+
+
+class BInt64(BaseStructField):
+    FORMAT = ">q"
+
+
+#===============================================================================
+# Unsigned Little Endian
+#===============================================================================
+class LUInt8(BaseStructField):
+    FORMAT = "<B"
+
+
+class LUInt16(BaseStructField):
+    FORMAT = "<H"
+
+
+class LUInt32(BaseStructField):
+    FORMAT = "<I"
+
+
+class LUInt64(BaseStructField):
+    FORMAT = "<Q"
+
+
+#===============================================================================
+# Signed Little Endian
+#===============================================================================
+class LInt8(BaseStructField):
+    FORMAT = "<b"
+
+
+class LInt16(BaseStructField):
+    FORMAT = "<h"
+
+
+class LInt32(BaseStructField):
+    FORMAT = "<i"
+
+
+class LInt64(BaseStructField):
+    FORMAT = "<q"
---
+++ pacman/message.py
@@ -0,0 +1,78 @@
+from pacman.fields import BaseField
+from itertools import izip
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+
+class Packer(object):
+
+    def __init__(self, ordered_fields):
+        self.ordered_fields = ordered_fields
+        self.ordered_field_lengths = [len(f) for _n, f in ordered_fields]
+        self.total_length = sum(self.ordered_field_lengths)
+
+    def pack(self):
+        # use a StringIO with the right length, starting from the beginning
+        sio = StringIO()
+
+        for _name, field in self.ordered_fields:
+            sio.write(field.pack())
+        return sio.getvalue()
+
+    def unpack(self, data):
+        if len(data) != self.total_length:
+            raise ValueError, "Expected data to have length %s, had %s" % (self.total_length, len(data))
+        pos = 0
+        for (_name, field), length in izip(self.ordered_fields, self.ordered_field_lengths):
+            new_pos = pos + length
+            data_seg = data[pos:new_pos]
+            pos = new_pos
+            field.unpack(data_seg)
+
+
+class MessageMeta(type):
+
+    def __new__(cls, name, bases, dct):
+        cls._fields = {}
+        for key, value in dct.iteritems():  # use a copy, we mutate dct
+            if isinstance(value, BaseField):
+                cls._fields[key] = value
+        sorted_fields = list(sorted(cls._fields.items(),
+                                   cmp=lambda (k1, v1), (k2, v2): cmp(v1._field_seqno,
+                                                                      v2._field_seqno)))
+        dct['_sorted_fields'] = sorted_fields
+
+        # create a "packer" object for the message.  This is allows us
+        # to take advantage of optimizations from stringing fields
+        # together at class creation time.  It also prevents us from
+        # having to do extra work at runtime on data that is only
+        # modified once (at class creation time)
+        dct['_packer'] = Packer(sorted_fields)
+
+        return type.__new__(cls, name, bases, dct)
+
+
+class BaseMessage(object):
+
+    __metaclass__ = MessageMeta
+
+    def __len__(self):
+        return sum(len(f) for _n, f in self)
+
+    def __iter__(self):
+        return iter(self._sorted_fields)
+
+    def __repr__(self):
+        output = "%s (\n" % self.__class__.__name__
+        for field_name, field in self:
+            output += "  %s=%s,\n" % (field_name, field)
+        output += ")"
+        return output
+
+    def unpack(self, data):
+        return self._packer.unpack(data)
+
+    def pack(self):
+        return self._packer.pack()
