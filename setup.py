e7c90fc9269e54bb9f0e1e5c31b15deda4aed4b5
Paul Osborne <Paul.Osborne@digi.com>
2012-02-20 21:40:31 -0600

    Change overview to use definition lists

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import find_packages
 setup(
   name='pacman',
   version='0.1.1',
+  description='A library for specifying/parsing binary protocols',
+  long_description=open('README.txt').read(),
   author="Paul Osborne",
   author_email="paul.osborne@spectrumdsi.com",
   packages=find_packages(),
